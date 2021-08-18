78de6510904f38ce638b45e3fb5700704864988b
Сергей Письменный <8alig8@gmail.com>
2021-08-18 12:07:19 +0300

    🎉 Initial commit.

---
+++ src/Monq.Core.Authorization.Models/PacketOwnerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monq.Core.Authorization.Models
+{
+    /// <summary>
+    /// Модель представления владельцев пакета пользовательских прав.
+    /// </summary>
+    public sealed class PacketOwnerViewModel
+    {
+        /// <summary>
+        /// Идентификатор пакета прав.
+        /// </summary>
+        public long PacketId { get; set; }
+
+        /// <summary>
+        /// Идентификатор рабочей группы-владельца пакета.
+        /// </summary>
+        public long WorkGroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользовательского пространства.
+        /// </summary>
+        public long UserspaceId { get; set; }
+
+        /// <summary>
+        /// Коллекция идентификаторов пользователей пакета.
+        /// </summary>
+        public IEnumerable<long> Users { get; set; } = Array.Empty<long>();
+    }
+}
---
+++ src/Monq.Core.Authorization.Models/SystemPacketMapViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Monq.Core.Authorization.Models
+{
+    /// <summary>
+    /// Представление модели соответствия системных пакетов прав.
+    /// </summary>
+    public sealed class SystemPacketMapViewModel
+    {
+        /// <summary>
+        /// Идентификатор записи соответствия.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор пространства.
+        /// </summary>
+        public long UserspaceId { get; set; }
+
+        /// <summary>
+        /// Идентификатор системного пакета.
+        /// </summary>
+        public long PacketId { get; set; }
+
+        /// <summary>
+        /// Тип пакета.
+        /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PacketTypes PacketType { get; set; }
+    }
+}
