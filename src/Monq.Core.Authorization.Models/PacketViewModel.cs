d9d37a56685623e9aeebac6d75d056234946ce93
Sergey Pismennyi <8alig8@gmail.com>
2026-07-14 13:20:57 +0300

    Change TargetFrameworks to net8.0;net9.0;net10.0 for better trimming support
    - Add <IsAotCompatible>true</IsAotCompatible> attribute
    - Change sln -> slnx

--- src/Monq.Core.Authorization.Models/PacketViewModel.cs
+++ src/Monq.Core.Authorization.Models/PacketViewModel.cs
@@ -1,46 +1,45 @@
 using System;
 using System.Collections.Generic;
 
-namespace Monq.Core.Authorization.Models
+namespace Monq.Core.Authorization.Models;
+
+/// <summary>
+/// Модель представления пакета пользовательских прав.
+/// </summary>
+public sealed class PacketViewModel
 {
     /// <summary>
-    /// Модель представления пакета пользовательских прав.
+    /// Идентификатор пакета пользовательских прав.
     /// </summary>
-    public sealed class PacketViewModel
-    {
-        /// <summary>
-        /// Идентификатор пакета пользовательских прав.
-        /// </summary>
-        public long Id { get; set; }
+    public long Id { get; set; }
 
-        /// <summary>
-        /// Имя пакета пользовательских прав.
-        /// </summary>
-        public string Name { get; set; } = default!;
+    /// <summary>
+    /// Имя пакета пользовательских прав.
+    /// </summary>
+    public string Name { get; set; } = default!;
 
-        /// <summary>
-        /// Описание пакета пользовательских прав.
-        /// </summary>
-        public string? Description { get; set; }
+    /// <summary>
+    /// Описание пакета пользовательских прав.
+    /// </summary>
+    public string? Description { get; set; }
 
-        /// <summary>
-        /// Является ли пакет доступным только для чтения.
-        /// </summary>
-        public bool IsReadOnly { get; set; }
+    /// <summary>
+    /// Является ли пакет доступным только для чтения.
+    /// </summary>
+    public bool IsReadOnly { get; set; }
 
-        /// <summary>
-        /// Коллекция прав доступа пакета.
-        /// </summary>
-        public IEnumerable<string> Grants { get; set; } = Array.Empty<string>();
+    /// <summary>
+    /// Коллекция прав доступа пакета.
+    /// </summary>
+    public IEnumerable<string> Grants { get; set; } = Array.Empty<string>();
 
-        /// <summary>
-        /// Коллекция владельцев пакета.
-        /// </summary>
-        public IEnumerable<PacketOwnerViewModel> Owners { get; set; } = Array.Empty<PacketOwnerViewModel>();
+    /// <summary>
+    /// Коллекция владельцев пакета.
+    /// </summary>
+    public IEnumerable<PacketOwnerViewModel> Owners { get; set; } = Array.Empty<PacketOwnerViewModel>();
 
-        /// <summary>
-        /// Тип пакета - пакет рабочей группы или пакет пространства.
-        /// </summary>
-        public PacketTypes Type { get; set; }
-    }
+    /// <summary>
+    /// Тип пакета - пакет рабочей группы или пакет пространства.
+    /// </summary>
+    public PacketTypes Type { get; set; }
 }
